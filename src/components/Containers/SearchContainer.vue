3a5355f468389d42869b7ec936c3ad598aa0990a
yjkim95 <yjkim95@douzone.com>
2020-09-11 17:52:07 +0900

    - navigation/container

---
+++ src/components/Containers/SearchContainer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="search-container-wrapper">
+    <div class="search-container-header">
+      {{ title }}
+    </div>
+    <div class="search-container-content">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'search-container-wrapper',
+    data() {
+      return {
+        title: '수주등록',
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .search-container-wrapper{
+    min-height: 200px;
+    padding: 0;
+
+    .search-container-header {
+      width: 100%;
+      height: 50px;
+      background-color: #e9ecef;
+    }
+
+    .search-container-content {
+      height: 150px;
+      background-color: #e0e0e0;
+    }
+  }
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navigation-bar"></div>
+</template>
+
+<script>
+  export default {
+    name: 'navigation-bar'
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    padding: 0;
+  }
+  .navigation-bar {
+    position: absolute;
+    width: 100%;
+    height: 44px;
+    background-color: #1c90fb;
+    top: 0;
+  }
+</style>
