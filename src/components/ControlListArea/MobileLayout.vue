ee03779edcce065fab22765b9a21625033e21722
yjkim95 <yjkim95@douzone.com>
2020-10-13 17:59:20 +0900

    - Mobile Layout Select scss 추가

--- src/components/ControlListArea/MobileLayout.vue
+++ src/components/ControlListArea/MobileLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="controlList-mobile-layout">
-    <div class="mobile-layout smartPhone-layout smartPhone" @click="selectLayout($event)">
+    <div :class="chkPhone" class="mobile-layout smartPhone" @click="selectLayout($event)">
       <div class="mobile-image smartPhone-image smartPhone"></div>
     </div>
-    <div class="mobile-layout tabletM-layout tabletM" @click="selectLayout($event)">
+    <div :class="chkTabletM" class="mobile-layout tabletM" @click="selectLayout($event)">
       <div class="mobile-image tabletM-image tabletM"></div>
     </div>
-    <div class="mobile-layout tabletL-layout tabletL" @click="selectLayout($event)">
+    <div :class="chkTabletL" class="mobile-layout tabletL" @click="selectLayout($event)">
       <div class="mobile-image tabletL-image tabletL"></div>
     </div>
   </div>
@@ -17,7 +17,9 @@
     name: 'mobile-layout',
     data() {
       return {
-
+        chkPhone: 'off',
+        chkTabletM: 'off',
+        chkTabletL: 'on'
       }
     },
     methods: {
@@ -30,18 +32,27 @@
             designer: 'designer-smartPhone',
             layout: 'smartPhone'
           })
+          this.chkPhone = 'on';
+          this.chkTabletM = 'off';
+          this.chkTabletL = 'off';
         } else if (e.target.classList.contains('tabletM')){
           this.$store.commit('setLayout', {
             wrapper: 'designer-wrapper-tabletM',
             designer: 'designer-tabletM',
             layout: 'tabletM'
           })
+          this.chkPhone = 'off';
+          this.chkTabletM = 'on';
+          this.chkTabletL = 'off';
         } else if (e.target.classList.contains('tabletL')){
           this.$store.commit('setLayout', {
             wrapper: 'designer-wrapper-tabletL',
             designer: 'designer-tabletL',
             layout: 'tabletL'
           })
+          this.chkPhone = 'off';
+          this.chkTabletM = 'off';
+          this.chkTabletL = 'on';
         }
       },
     }
@@ -59,8 +70,8 @@
 
     .mobile-layout {
       display: inline-block;
+      width: 50px;
       height: 36px;
-      background-color: #4e5c7b;
       cursor: pointer;
 
       .mobile-image {
@@ -71,25 +82,45 @@
       }
     }
 
-    .smartPhone-layout {
-      width: 50px;
+    .on {
+      background-color: #35425f;
+    }
 
+    .off {
+      background-color: #4e5c7b;
+    }
+
+    .smartPhone.on {
+      .smartPhone-image {
+        background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAAXnVPIAAAAX0lEQVQ4EWNgGGjAiO6AgMN/LwDF9NHFofwLG2yZDXHIQYSBBvzHpQCbHBMuxcSKjxrAwDDwYcCCLbqwxTc2dSAx/F5gYVJkAGE8AJsBF+Hq//y7zwDCCICQQ4gNMAsASfsV5+Q/z8sAAAAASUVORK5CYII=');
+      }
+    }
+
+    .smartPhone.off {
       .smartPhone-image {
         background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAAXnVPIAAAASUlEQVQ4EWNgGHTg////F4AYFzhP0MEgnbgUYZNjwqWYWPFRAxgYBj4MWLBFF7b4xqYOJEbIC4pANSBMPADaji8pXyDeJHqpBABsbktOzkcElwAAAABJRU5ErkJggg==');
       }
     }
 
-    .tabletM-layout {
-      width: 50px;
+    .tabletM.on {
+      .tabletM-image {
+        background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAAXnVPIAAAAWUlEQVQ4EWNkgIKAw38vAJn6MD4B+sIGW2ZDFDVAA/6jCODhIKtlwqOOKKlRAxgYRsNgMIQBC3J6RU7jyOL42PijkYVJkQGE8QBkAy5iqPvz7z4DCGMCuFoAsXcV7yVkiOMAAAAASUVORK5CYII=');
+      }
+    }
 
+    .tabletM.off {
       .tabletM-image {
         background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAAXnVPIAAAASklEQVQ4EWNggIL///9fAGJiwXmYPjgN0gnnEGAgq2UioJag9KgBDAyjYTAYwoAFOa0ip3FkcXxsQtGoCNQMwoQB0HZSsvMFmIkAQnFrNpWqZUIAAAAASUVORK5CYII=');
       }
     }
 
-    .tabletL-layout {
-      width: 50px;
+    .tabletL.on {
+      .tabletL-image {
+        background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAAXnVPIAAAATklEQVQ4EWMMOPz3AgMDgz4QkwMuMAAN+E+OTpAekF4mcjXD9I0awMAwGgaDIQwYKckLoOSMPxpZmBQZQBgPABlwEaf8n3/3GUAYN7gIAJC9FMuG9rVhAAAAAElFTkSuQmCC');
+      }
+    }
 
+    .tabletL.off {
       .tabletL-image {
         background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAADhlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAAqACAAQAAAABAAAAEKADAAQAAAABAAAAEAAAAAAXnVPIAAAARUlEQVQ4EWP8////BQYGBn0gJgdcYAAa8J8cnSA9IL1M5GqG6Rs1gIFhNAwGQxgwUpIXQMmZUDQqAtWAME4AcgEl2fkiAOTtFeukJYGHAAAAAElFTkSuQmCC');
       }
