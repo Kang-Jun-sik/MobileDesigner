88cb0f3d312b9836a377025ba834b07f1ad5e34c
kotiger <kotiger@douzone.com>
2020-11-02 10:46:16 +0900

    khj - form/search container 1차 완료

--- src/components/Containers/FormContainer.vue
+++ src/components/Containers/FormContainer.vue
@@ -3,18 +3,93 @@
     <div class="dews-container-option-control">
       <h3 class="option-sub-title" v-if="title">{{ title }}</h3>
 
-      <div v-if="customButton.length" class="option-custom-button">
-        <ul></ul>
+<!--      <div v-if="customButton.length" class="option-custom-button">-->
+      <div class="option-custom-button">
+        <ul>
+          <li><button><span>확정</span></button></li>
+          <li><button><span>확정 취소</span></button></li>
+          <li><button><span>종결</span></button></li>
+          <li><button><span>종결 취소</span></button></li>
+          <li><button><span>확정</span></button></li>
+          <li><button><span>확정 취소</span></button></li>
+          <li><button><span>종결</span></button></li>
+        </ul>
       </div>
 
       <div class="option-convenience-button">
-        <ul></ul>
+        <ul>
+          <li class="data-capture"><button class="capture"><span> Data Capture</span></button></li>
+          <li class="data-set"><button class="set"><span>Data Set</span></button></li>
+          <li class="data-reset"><button class="reset"><span>Data Reset</span></button></li>
+        </ul>
       </div>
     </div>
 
-    <div class="dews-search-field">
-      <ul class="form-field"></ul>
+    <div class="form-section">
+      <h3 class="dews-form-section-title">기본정보</h3>
+      <div class="dews-form-field">
+        <ul class="form-field">
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
+
+    <div class="form-section">
+      <h3 class="dews-form-section-title">추가정보</h3>
+      <div class="dews-form-field">
+        <ul class="form-field">
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+          <li>
+            <div class="dews-textbox">
+              <label for="">항목명</label>
+              <input type="text" id="textbox" class="">
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -37,5 +112,68 @@
 </script>
 
 <style lang="scss" scoped>
+@import 'node_modules/@dews/dews-mobile-style/scss/variables/variables';
+@import 'node_modules/@dews/dews-mobile-style/scss/mixins/_mixins';
+
+//======================================
+// dews-form-container
+//======================================
+//--------------------------------------
+// 레이아웃 영역
+//--------------------------------------
+* {
+  @include reset();
+}
+//--------------------------------------
+// dews-container-option-bar
+//--------------------------------------
+.dews-container-option-control {
+  @include option-control();
+}
+
+.dews-form-section-title {
+  @include container-title-bar($base-color-bar); //디자인 수정 이슈 , 색상 코드 정의 필요
+}
+.dews-form-field {
+  @include form-field();
+}
+
+
+//임시 button component 모양 잡기
+button {
+  border-radius: 4px;
+  border: solid 1px #d5d5d5;
+  background-color: #ffffff;
+
+  padding: 4px 10px;
+  font-size: 13px;
+  line-height: 1.54;
+  letter-spacing: normal;
+  text-align: center;
+  color: rgba(60, 60, 67, 0.8);
+}
+//임시 input component 모양 잡기
+label {
+  display: block;
+  width: 100%;
+  margin-bottom: 2px;
+  font-size: 12px;
+  line-height: 1.5;
+  text-align: left;
+  color: rgba(60, 60, 67, 0.6);
+}
+input[type="text"] {
+  position: relative;
+  width: 100%;
+  height: 36px;
+  padding: 7px 10px;
+  color: rgb(17, 17, 17);
+  border: 1px solid rgba(60, 60, 67, 0.08);
+  border-radius: 4px;
+  background-color: rgb(255, 255, 255);
+  font-size: 15px;
+  line-height: 1.47;
+  text-align: left;
+}
 
 </style>
--- src/components/Containers/SearchContainer.vue
+++ src/components/Containers/SearchContainer.vue
@@ -2,19 +2,47 @@
   <div :uid="uid" class="dews-mobile-searchContainer">
     <div class="dews-container-option-control">
       <h3 class="option-sub-title" v-if="title">{{ title }}</h3>
-      <div v-if="customButton.length" class="option-convenience-button">
+<!--      <div v-if="customButton.length" class="option-convenience-button">-->
+      <div class="option-convenience-button">
         <ul>
-          <li v-for="(custom, idx) in customButton" :key="idx" :class="custom.type">{{ custom.button }}</li>
+<!--          <li v-for="(custom, idx) in customButton" :key="idx" :class="custom.type">{{ custom.button }}</li>-->
+          <li class="data-capture"><button class="capture"><span> Data Capture</span></button></li>
+          <li class="data-set"><button class="set"><span>Data Set</span></button></li>
+          <li class="data-reset"><button class="reset"><span>Data Reset</span></button></li>
         </ul>
       </div>
     </div>
 
     <div class="dews-search-field">
       <ul class="form-field">
-        <slot v-if="this.inputList">
-          <li v-for="(input, idx) in inputList" :key="idx"> {{ input }}</li>
-        </slot>
-        <slot v-else></slot>
+<!--        <slot v-if="this.inputList">-->
+<!--          <li v-for="(input, idx) in inputList" :key="idx"> {{ input }}</li>-->
+<!--        </slot>-->
+<!--        <slot v-else></slot>-->
+        <li>
+          <div class="dews-textbox">
+            <label for="">항목명</label>
+            <input type="text" id="textbox" class="">
+          </div>
+        </li>
+        <li>
+          <div class="dews-textbox">
+            <label for="">항목명</label>
+            <input type="text" id="textbox" class="">
+          </div>
+        </li>
+        <li>
+          <div class="dews-textbox">
+            <label for="">항목명</label>
+            <input type="text" id="textbox" class="">
+          </div>
+        </li>
+        <li>
+          <div class="dews-textbox">
+            <label for="">항목명</label>
+            <input type="text" id="textbox" class="">
+          </div>
+        </li>
       </ul>
     </div>
   </div>
@@ -44,5 +72,58 @@
 </script>
 
 <style lang="scss" scoped>
+@import 'node_modules/@dews/dews-mobile-style/scss/variables/variables';
+@import 'node_modules/@dews/dews-mobile-style/scss/mixins/_mixins';
+
+//======================================
+// dews-search-container
+//======================================
+//--------------------------------------
+// 레이아웃 영역
+//--------------------------------------
+* {
+  @include reset();
+}
+
+
+//--------------------------------------
+// dews-container-option-bar
+//--------------------------------------
+.dews-container-option-control {
+  @include option-control();
+}
+
+
+.dews-search-field{
+  @include form-field();
+
+  :host([col='2']) & {
+    @include form-field(2);
+  }
+}
+
+//임시 input component 모양 잡기
+label {
+  display: block;
+  width: 100%;
+  margin-bottom: 2px;
+  font-size: 12px;
+  line-height: 1.5;
+  text-align: left;
+  color: rgba(60, 60, 67, 0.6);
+}
+input[type="text"] {
+  position: relative;
+  width: 100%;
+  height: 36px;
+  padding: 7px 10px;
+  color: rgb(17, 17, 17);
+  border: 1px solid rgba(60, 60, 67, 0.08);
+  border-radius: 4px;
+  background-color: rgb(255, 255, 255);
+  font-size: 15px;
+  line-height: 1.47;
+  text-align: left;
+}
 
 </style>
