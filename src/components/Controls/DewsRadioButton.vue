8accb883d88f7853d26ce67e4de0abe4f203d53d
yejikk <dpwl7484@gmail.com>
2020-11-30 16:40:05 +0900

    dews-mobile-radiobutton 로직 구현

---
+++ src/components/Controls/DewsRadioButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <span :uid="uid" class="dews-mobile-radiobutton dews-radio-wrap" @click="clickHandler($event)">
+    <span class="radio-control">
+      <input type="radio" :checked="checked" :disabled="disabled">
+      <span class="radio-shape"></span>
+    </span>
+    <label class="radio-label">{{ title }}</label>
+  </span>
+</template>
+
+<script>
+import ControlService from "@/service/ControlService";
+
+export default {
+  name: 'dews-radio-button',
+  data() {
+    return {
+      uid: '',
+      title: '',
+      checked: false,
+      disabled: false,
+      readonly: false,
+    }
+  },
+  created() {
+    this.uid = ControlService.createUid('dews-radiobutton');
+  },
+  mounted() {
+  },
+  methods: {
+    clickHandler (e){
+      e.stopPropagation();
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
