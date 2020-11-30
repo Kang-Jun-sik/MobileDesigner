5640c110fb4d1291ac78c201dfede4c385522bff
yejikk <dpwl7484@gmail.com>
2020-11-30 16:39:34 +0900

    dews-mobile-checkbox 로직 구현

---
+++ src/components/Controls/DewsCheckBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <span :uid="uid" class="dews-mobile-checkbox" :class="checkBoxClass" @click="clickHandler($event)">
+    <span class="checkbox-control">
+      <input type="checkbox" :checked="checked" :disabled="disabled">
+      <span class="checkbox-shape"></span>
+    </span>
+    <label class="checkbox-label">{{ title }}</label>
+  </span>
+</template>
+
+<script>
+import ControlService from "@/service/ControlService";
+
+export default {
+  name: 'dews-checkbox',
+  data() {
+    return {
+      uid: '',
+      checkBoxClass: '',
+      title: '',
+      checked: false,
+      disabled: false,
+    }
+  },
+  created() {
+    this.uid = ControlService.createUid('dews-checkbox');
+  },
+  mounted() {
+  },
+  methods: {
+    clickHandler (e){
+      e.stopPropagation();
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
