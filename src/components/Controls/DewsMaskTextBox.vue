e01509819e110d47cfab027583522a943664b400
yjkim95 <yjkim95@douzone.com>
2020-12-09 14:42:42 +0900

    - 컴포넌트 생성 진행중

---
+++ src/components/Controls/DewsMaskTextBox.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="mask-textbox-wrap">
+    <label>{{ title }}</label>
+    <span @click="onClick()" class="view" :class="disabled ? 'disabled' : ''">
+      <input type="text" @focus="onClick()"
+             :disabled="disabled" :readonly="readonly"
+             :value="value" :placeholder="placeholder">
+    </span>
+    <span class="mask" style="display: none;">
+      <input type="text" @focus="onFocus()" @blur="onBlur()">
+    </span>
+    <span class="input-state" :class="stateType">{{ stateMessage }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dews-maskbox',
+  data() {
+    return {
+      title: 'title',
+      placeholder: '',
+      format: '',
+      type: 'text',
+      disabled: false,
+      readonly: false,
+      required: false,
+      mask: '',
+      prompt: '_',
+      unmaskOnPost: true,
+      value: '',
+      raw: '',
+      stateType: '',
+      stateMessage: '',
+    }
+  },
+  created() {
+  },
+  methods: {
+    onClick() {
+
+    },
+    onFocus() {
+
+    },
+    onChange() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Controls/DewsNumericTextBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="numeric-textbox-wrap" :class="{disabled: disabled ? 'disabled' : '', readonly: readonly ? 'readonly': ''}">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dews-numerictextbox',
+  data() {
+    return {
+      uid: '',
+      title: 'title',
+      placeholder: '',
+      disabled: false,
+      readonly: false,
+      required: false,
+      value: '',
+      prefix: '',
+      suffix: '',
+      format: '#,##0.00',
+      decimals: null,
+      max: null,
+      min: null,
+      restrict: false,
+      maxLength: null,
+      round: 'round'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
