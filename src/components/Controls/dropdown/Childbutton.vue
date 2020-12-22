7b6eac2fe6c6544c82e499453fbfd3885c9f90b6
yjkim95 <yjkim95@douzone.com>
2020-12-22 09:20:44 +0900

    - Dropdown button, Dropdown child button 컴포넌트 추가

---
+++ src/components/Controls/dropdown/Childbutton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button :uid="uid" class="button-list-child" :disabled="disabled">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+import CreateService from "@/service/CreateService";
+
+export default {
+  name: 'dews-dropdown-childbutton',
+  data() {
+    return {
+      uid: '',
+      text: '',
+      disabled: false
+    }
+  },
+  created() {
+    this.uid = CreateService.createUid('dropdown-childbutton');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Controls/dropdown/DewsDropdownbutton.vue
@@ -0,0 +1,56 @@
+<template>
+  <span :uid="uid" class="dews-dropdown-button" :class="{ group: group ? 'group' : '' }">
+    <button @click="clickHandler($event)" class="dews-button dropdown" :class="{ui, size, disabled: disabled ? 'disabled' : ''}">
+      <span class="button-icon"></span>
+      <span class="button-text">{{ text }}</span>
+    </button>
+    <span class="button-list" :class="{ selected: selected ? 'selected' : '' }">
+      {{ childButtons }}
+    </span>
+  </span>
+</template>
+
+<script>
+import CreateService from "@/service/CreateService";
+
+export default {
+  name: 'dews-dropdownbutton',
+  data() {
+    return {
+      uid: '',
+      text: '',
+      SIZE_LIST: {
+        small: 'small',
+        medium: 'medium',
+        large: 'large',
+      },
+      UI_LIST: {
+        solid: 'solid',
+        emphasize: 'emphasize'
+      },
+      ui: '',
+      size: '',
+      disabled: false,
+      group: false,
+      selected: false,
+      childButtons: [],
+    }
+  },
+  created() {
+    this.uid = CreateService.createUid('dews-dropdownbutton');
+  },
+  methods: {
+    clickHandler(e) {
+      if (!this.disabled) {
+        this.selected = false;
+      } else {
+        this.selected = e.target === this.$el;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
