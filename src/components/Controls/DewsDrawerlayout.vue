861a932ab90e0ab27678b8560f72a52ec3102eb4
yejikk <dpwl7484@gmail.com>
2020-12-28 09:13:30 +0900

    - 컴포넌트 추가

---
+++ src/components/Controls/DewsDrawerlayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="layer-drawer">
+    <div class="overlay"></div>
+    <div class="layer layer-bottom" :style="style">
+      <div class="layer-moving-button">
+        <span v-if="scrollEnabled" class="moving-button"></span>
+        <span v-else class="fixed-button"></span>
+      </div>
+      <div class="layer-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'drawer-layout',
+  data() {
+    return {
+      title: '',
+      active: false,
+      height: '',
+      scrollEnabled: false,
+      drawer: 'bottom',
+      style: {
+        height: '0px'
+      },
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    touchMove() {
+
+    },
+    touchStart() {
+
+    },
+    touchEnd() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Controls/DewsTimePicker.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="time-picker-wrap" @click="clickHandler">
+      <label>{{ title }}</label>
+      <span class="select-wrap">
+        <span class="select-shape">
+          <span class="select-input">{{ inputValue }}</span>
+        </span>
+        <span class="select-icon time-picker-icon"></span>
+      </span>
+    </div>
+
+<!--    <drawer-layout>-->
+<!--      <div></div>-->
+<!--    </drawer-layout>-->
+  </div>
+</template>
+
+<script>
+import DrawerLayout from "@/components/Controls/DewsDrawerlayout";
+
+export default {
+  name: 'dews-timepicker',
+  // components: {DrawerLayout},
+  data() {
+    return {
+      uid: '',
+      title: '',
+      inputValue: '',
+      disabled: false,
+      readonly: false,
+      height: '',
+      value: '',
+      min: '',
+      max: '',
+      step: 1,
+    }
+  },
+  created() {
+  },
+  methods: {
+    clickHandler() {
+
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
