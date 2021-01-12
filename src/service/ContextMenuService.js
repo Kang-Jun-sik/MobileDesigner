ca8964796e6884dae7c428c4535c91962133a28e
yjkim95 <yjkim95@douzone.com>
2021-01-12 13:37:14 +0900

    - ContextMenu 수정 진행중

--- src/service/ContextMenuService.js
+++ src/service/ContextMenuService.js
@@ -47,8 +47,8 @@ export default {
                 $.contextMenu({
                     selector: ".selected-control",
                     items: {
-                        "vertical": {name: "세로분할", icon: "edit"},
-                        "delete": {name: "Delete", icon: "delete"}
+                        "vertical": {name: "세로분할", icon: "ic-split"},
+                        "delete": {name: "Delete", icon: "ic-delete"}
                     },
                     callback: function(itemKey, opt) {
                         switch (itemKey){
@@ -60,16 +60,18 @@ export default {
                                 break;
                         }
                     }
-                })
+                });
                 break;
-
-            case "dews-mobile-searchContainer" :
+            case 'dews-mobile-tabs':
+                break;
+            case 'dews-mobile-tab':
+                break;
+            case 'dews-mobile-searchContainer':
                 $.contextMenu({
                     selector: ".selected-control",
                     items: {
-                        "edit": {name: "Search-container-option", icon: "edit"},
+                        "edit": {name: "Option", icon: "edit"},
                         "delete": {name: "Delete", icon: "delete"},
-                        "sep1": "---------",
                     },
                     callback: function(itemKey, opt) {
                         switch (itemKey){
@@ -80,14 +82,23 @@ export default {
                     }
                 });
                 break;
-
-            case "dews-mobile-button" :
+            case 'dews-mobile-listContainer':
+                break;
+            case 'dews-mobile-formContainer':
+                break;
+            case 'dews-mobile-infoContainer':
+                break;
+            case 'dews-mobile-formSection':
+                break;
+            case 'dews-mobile-containerContent':
+                break;
+            case 'dews-mobile-containerButton':
+                break;
+            case 'dews-mobile-button':
                 $.contextMenu({
                     selector: ".selected-control",
                     items: {
-                        "b1": {name: "ButtonOption1", icon: "edit"},
                         "delete": {name: "Delete", icon: "delete"},
-                        "sep1": "---------",
                     },
                     callback: function(itemKey, opt) {
                         switch (itemKey){
@@ -97,6 +108,38 @@ export default {
                         }
                     }
                 });
+                break;
+            case 'dews-mobile-buttonGroup':
+                break;
+            case 'dews-mobile-checkbox':
+                break;
+            case 'dews-checkbox-group':
+                break;
+            case 'dews-mobile-complex':
+                break;
+            case 'dews-mobile-datePicker':
+                break;
+            case 'dews-mobile-maskbox':
+                break;
+            case 'dews-mobile-monthPicker':
+                break;
+            case 'dews-mobile-numeric':
+                break;
+            case 'dews-mobile-periodPicker':
+                break;
+            case 'dews-mobile-radiobutton':
+                break;
+            case 'dews-mobile-radioGroup':
+                break;
+            case 'dews-mobile-textbox':
+                break;
+            case 'dews-mobile-timePicker':
+                break;
+            case 'dews-mobile-dropdownList':
+                break;
+            case 'dews-dropdownbuttondews-mobile-dropdownButton':
+                break;
+            case 'button-list-child':
                 break;
         }
     },
