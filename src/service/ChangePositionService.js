05ed9c66826a18e577e444535dde49f3f5345f6b
강준식 <kjs1436@douzone.com>
2021-01-22 11:25:07 +0900

    - Change Position 함수(createPositionInfo) - undo/redo 처리까지 개발 완료

--- src/service/ChangePositionService.js
+++ src/service/ChangePositionService.js
@@ -1,3 +1,4 @@
+import store from "@/store/index";
 import mobileDesignerToIDE from "@/utils/mobileDesignerToIDE";
 
 export default {
@@ -8,5 +9,23 @@ export default {
         const parent = component.parentElement.closest('.dews-mobile-component')
         const parentUid = parent.getAttribute('uid');
         mobileDesignerToIDE("change_control", component, parentUid);
+    },
+    /**
+     * 컨트롤 위치 정보 메세지 전달 함수 (IDE --> Mobile Designer)
+     */
+    changePositionServiceFromIDE(args) {
+        const obj = JSON.parse(args);
+        const idx = obj["index"];
+        const parentUid = obj["parentId"];
+        const controlUid = obj["uid"];
+
+        let parent = store.state.component.items.find(item => {
+            return item.uid === parentUid;
+        })
+        const control = store.state.component.items.find(item => {
+            return item.uid === controlUid;
+        })
+        document.querySelector(`[uid=${controlUid}]`).remove();
+        parent.$el.insertBefore(control.$el, parent.$el.children[idx]);
     }
 }
--- src/service/RegisterService.js
+++ src/service/RegisterService.js
@@ -1,22 +1,20 @@
-import GlobalService from "@/service/GlobalService";
-import UndoRedoService from "@/service/UndoRedoService";
 import PasteService from "@/service/PasteService";
 import CreateService from "@/service/CreateService";
 import DeleteService from "@/service/DeleteService";
 import PageOpenService from "@/service/PageOpenService";
 import SelectService from "@/service/SelectService";
 import ChangeService from "@/service/ChangeService";
+import ChangePositionService from "@/service/ChangePositionService";
 
 export default {
     init() {
         window.services = new Map();// 글로벌 서비스를 위한 서비스 Map 생성
-
         window.services.set(PageOpenService.openService.name, PageOpenService.openService); //Page Open(EWP 열기)
         window.services.set(SelectService.selectFromIDE.name, SelectService.selectFromIDE); //Control Selection(컨트롤 선택 from IDE)
-        //window.services.set(UndoRedoService.undoredoService.name, UndoRedoService.undoredoService); //Undo & Redo(실행취소&되돌리기) => Create Delete로 변경
         window.services.set(CreateService.createFromIDE.name, CreateService.createFromIDE); //Item Create From IDE (undo/redo)
         window.services.set(DeleteService.deleteFromIDE.name, DeleteService.deleteFromIDE); //Item Delete From IDE (undo/redo)
+        window.services.set(ChangePositionService.changePositionServiceFromIDE.name, ChangePositionService.changePositionServiceFromIDE); //Item Position Change From IDE (undo/redo)
         window.services.set(PasteService.pasteFromIDE.name, PasteService.pasteFromIDE); //Paste(붙여넣기)
-        window.services.set(ChangeService.changeFromIDE.name, ChangeService.changeFromIDE) //Control Change(컨트롤 속성 변경 from IDE)
+        window.services.set(ChangeService.changeFromIDE.name, ChangeService.changeFromIDE); //Control Change(컨트롤 속성 변경 from IDE)
     }
 }
