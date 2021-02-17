c613ce22a1b6624bb7c309db77ee8904ad83e682
강준식 <kjs1436@douzone.com>
2021-02-17 09:28:50 +0900

    - store 정리

--- src/store/modules/component.js
+++ src/store/modules/component.js
@@ -9,26 +9,12 @@ export default {
         dewsCardList: {}
     },
     getters: {
-        getItems(state) {
-            return state.items;
-        },
-        getUndoItems(state){
-            return state.undoItems;
-        },
-        getRedoItems(state){
-            return state.redoItems;
-        },
+
     },
     actions: {},
     mutations: {
         ADD_ITEM(state, item) {
             state.items.push(item);
-        },
-        ADD_UNDO_ITEM(state, item) {
-            state.undoItems.push(item);
-        },
-        ADD_REDO_ITEM(state,item){
-            state.redoItems.push(item);
         },
         MATCH_UID(state, payload) {
             state.dragulaUid[payload.uid] = payload.dataUid;
